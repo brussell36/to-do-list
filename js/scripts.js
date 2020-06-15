2b6578380f08c721ec40dbad2ecdd0bd68ed5f3a
Ben Russell <benjaminrussell36@gmail.com>
2020-06-15 14:06:59 -0700

    Add contructor for Tasks and TaskList. Add methods for adding tasks to TaskList and assigning unique IDs.
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
 // UI Logic
+let taskList = new TaskList();
+
 $(document).ready(function() {
   $("#tasks").submit(function(event) {
     event.preventDefault();
-
+    const tasks = $("input#tasks").val();
+    let newTask = new Tasks(tasks);
+    taskList.addTask(newTask);
+    console.log(tasklist.tasks);
   });
 });
 
 // Business Logic
+function TaskList() {
+  this.tasks = [];
+  this.currentId = 0;
+}
+
+TaskList.prototype.addTask = function(task) {
+  task.id = this.assignId();
+  this.tasks.push(task);
+}
+
+TaskList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+function Tasks(tasks) {
+  this.tasks = tasks
+}
